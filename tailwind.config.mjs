8089eb608642a7dd04bc6994ae989f2401b11626
daimond113 <72147841+daimond113@users.noreply.github.com>
2024-11-13 21:47:11 +0100

    feat: add light mode support

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,15 +7,30 @@ export default {
 		extend: {
 			colors: {
 				body: {
-					bg: "#08060e",
-					text: "#d2bcff",
-					link: "#b18aff",
+					bg: {
+						DEFAULT: "#d5c7ff",
+						dark: "#08060e",
+					},
+					text: {
+						DEFAULT: "#461C9C",
+						dark: "#d2bcff",
+					},
+					link: {
+						DEFAULT: "#043279",
+						dark: "#b18aff",
+					},
 				},
 				logo: {
-					text: "#DCCCFF",
+					text: {
+						DEFAULT: "#29186A",
+						dark: "#DBADFF",
+					},
 				},
 				alt: {
-					text: "#a68cd9",
+					text: {
+						DEFAULT: "#140531",
+						dark: "#a68cd9",
+					},
 				},
 			},
 			width: {
@@ -30,10 +45,29 @@ export default {
 			typography: ({ theme }) => ({
 				main: {
 					css: {
-						"--tw-prose-body": theme("colors.body.text"),
-						"--tw-prose-headings": theme("colors.body.text"),
-						"--tw-prose-lead": theme("colors.violet[100]"),
-						"--tw-prose-links": theme("colors.body.link"),
+						"--tw-prose-body": theme("colors.body.text.DEFAULT"),
+						"--tw-prose-headings": theme("colors.body.text.DEFAULT"),
+						"--tw-prose-lead": theme("colors.violet[700]"),
+						"--tw-prose-links": theme("colors.body.link.DEFAULT"),
+						"--tw-prose-bold": theme("colors.violet[400]"),
+						"--tw-prose-counters": theme("colors.violet[800]"),
+						"--tw-prose-bullets": theme("colors.violet[800]"),
+						"--tw-prose-hr": theme("colors.violet[100]"),
+						"--tw-prose-quotes": theme("colors.violet[400]"),
+						"--tw-prose-quote-borders": theme("colors.violet[400]"),
+						"--tw-prose-captions": theme("colors.violet[800]"),
+						"--tw-prose-th-borders": theme("colors.violet[800]"),
+						"--tw-prose-td-borders": theme("colors.violet[800]"),
+						"--tw-prose-code": theme("colors.alt.text.DEFAULT"),
+						"--tw-prose-kbd": theme("colors.alt.text.DEFAULT"),
+					},
+				},
+				dark: {
+					css: {
+						"--tw-prose-body": theme("colors.body.text.dark"),
+						"--tw-prose-headings": theme("colors.body.text.dark"),
+						"--tw-prose-lead": theme("colors.violet[400]"),
+						"--tw-prose-links": theme("colors.body.link.dark"),
 						"--tw-prose-bold": theme("colors.violet[400]"),
 						"--tw-prose-counters": theme("colors.violet[300]"),
 						"--tw-prose-bullets": theme("colors.violet[300]"),
@@ -43,9 +77,8 @@ export default {
 						"--tw-prose-captions": theme("colors.violet[300]"),
 						"--tw-prose-th-borders": theme("colors.violet[300]"),
 						"--tw-prose-td-borders": theme("colors.violet[300]"),
-						"--tw-prose-code": theme("colors.alt.text"),
-						"--tw-prose-kbd": theme("colors.alt.text"),
-						"--tw-prose-lead": theme("colors.alt.text"),
+						"--tw-prose-code": theme("colors.alt.text.dark"),
+						"--tw-prose-kbd": theme("colors.alt.text.dark"),
 					},
 				},
 			}),
