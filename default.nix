e0735919b6072f378e163f594a981777f1854e8c
daimond113 <contact@daimond113.com>
2025-11-22 20:51:39 +0100

    chore: update dependencies, add nix

---
+++ default.nix
@@ -0,0 +1,34 @@
+{
+  stdenv,
+  bun2nix,
+  pkgs,
+  ...
+}:
+let
+  lib = pkgs.lib;
+  package = lib.importJSON ./package.json;
+in
+stdenv.mkDerivation {
+  pname = package.name;
+  version = package.version;
+
+  src = ./.;
+
+  nativeBuildInputs = [
+    bun2nix.hook
+  ];
+
+  bunDeps = bun2nix.fetchBunDeps {
+    bunNix = ./bun.nix;
+  };
+
+  buildPhase = ''
+    bun run build
+  '';
+
+  installPhase = ''
+    mkdir -p $out/dist
+
+    cp -R ./dist $out
+  '';
+}
---
+++ flake.nix
@@ -0,0 +1,54 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
+
+    bun2nix = {
+      url = "github:nix-community/bun2nix";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        systems.follows = "systems";
+      };
+    };
+  };
+  nixConfig = {
+    extra-substituters = [
+      "https://cache.nixos.org"
+      "https://nix-community.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+  outputs =
+    inputs:
+    let
+      eachSystem = inputs.nixpkgs.lib.genAttrs (import inputs.systems);
+      pkgsFor = eachSystem (
+        system:
+        import inputs.nixpkgs {
+          inherit system;
+          overlays = [ inputs.bun2nix.overlays.default ];
+        }
+      );
+    in
+    {
+      packages = eachSystem (system: {
+        default = pkgsFor.${system}.callPackage ./default.nix { pkgs = pkgsFor.${system}; };
+      });
+
+      devShells = eachSystem (system: {
+        default = pkgsFor.${system}.mkShell {
+          packages = with pkgsFor.${system}; [
+            bun
+            bun2nix
+          ];
+
+          shellHook = ''
+            bun install --frozen-lockfile
+          '';
+        };
+      });
+    };
+}
